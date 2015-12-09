6e6c2a6caf35e7a680acb2f53d87008cdee12f98
jricke <ricke.jason@gmail.com>
2015-12-08 22:19:48 -0600

    first projects

---
+++ .SolutionItems/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("NRevolution")]
+[assembly: AssemblyProduct("NRevolution Framework")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 NRevolution")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
